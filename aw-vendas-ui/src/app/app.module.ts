2d986df4bb2915d04653954b027262c289b01d0d
Renato Herebia <rherebia@gmail.com>
2018-07-09 00:15:12 -0400

    Alterações realizadas até 1:33:39 do vídeo 3.

---
+++ aw-vendas-ui/src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VendasListagemComponent } from './vendas-listagem/vendas-listagem.component';
+import { VendaCadastroComponent } from './venda-cadastro/venda-cadastro.component';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    VendasListagemComponent,
+    VendaCadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+
+    CurrencyMaskModule,
+    TableModule,
+    DropdownModule,
+    InputTextModule,
+    PanelModule,
+    ButtonModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ aw-vendas-ui/src/app/venda-cadastro/venda-cadastro.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { VendasService } from '../vendas/vendas.service';
+
+@Component({
+  selector: 'app-venda-cadastro',
+  templateUrl: './venda-cadastro.component.html',
+  styleUrls: ['./venda-cadastro.component.css']
+})
+export class VendaCadastroComponent implements OnInit {
+
+  clientes: Array<any>;
+  produtos: Array<any>;
+  venda: any = { itens: [] };
+  item: any = {};
+
+  constructor(private vendaService : VendasService) { }
+
+  ngOnInit() {
+    this.vendaService.listarClientes()
+      .subscribe(response => this.clientes = response);
+
+    this.vendaService.listarProdutos()
+      .subscribe(response => this.produtos = response);
+  }
+
+  incluirItem() {
+    this.item.total = this.item.produto.valor * this.item.quantidade;
+
+    this.venda.itens.push(this.item);
+
+    this.item = {};
+  }
+
+}
